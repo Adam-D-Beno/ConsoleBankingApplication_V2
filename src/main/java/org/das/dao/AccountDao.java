6cf364fb50423fbb352970189a9aea016b5f0e2c
ПК_Home <wrofdt@ya.ru>
2024-12-01 22:12:26 +0300

    refactoring AccountDao for JPA

--- src/main/java/org/das/dao/AccountDao.java
+++ src/main/java/org/das/dao/AccountDao.java
@@ -4,34 +4,48 @@ import org.das.model.Account;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import java.util.Collection;
-import java.util.Map;
-import java.util.Optional;
-import java.util.UUID;
+import java.util.*;
 
 @Repository
 public class AccountDao {
+    private final TransactionHelper transactionHelper;
     private final Map<UUID, Account> accounts;
 
     @Autowired
-    public AccountDao(Map<UUID, Account> accounts) {
+    public AccountDao(TransactionHelper transactionHelper, Map<UUID, Account> accounts) {
+        this.transactionHelper = transactionHelper;
         this.accounts = accounts;
     }
 
     public Account save(Account account) {
-        accounts.put(account.getAccountId(), account);
-        return account;
+        return transactionHelper.executeInTransaction(session -> {
+            session.persist(account);
+            return account;
+        });
     }
 
     public Collection<Account> getAllAccounts() {
        return accounts.values();
     }
 
-    public Optional<Account> getAccount(UUID id) {
-        return Optional.ofNullable(accounts.get(id));
+    public List<Account> getAccounts() {
+        return transactionHelper.executeInTransaction(session -> {
+           return session.createQuery("SELECT ac FROM Account ac", Account.class)
+                    .list();
+        });
     }
 
+    public Optional<Account> getAccount(UUID id) {
+        return Optional.ofNullable(transactionHelper.executeInTransaction(session -> {
+           return session.get(Account.class, id);
+        }));
+    }
+
+    //todo if not find account
     public void remove(UUID id) {
-        accounts.remove(id);
+        transactionHelper.executeInTransaction(session -> {
+            Account account = session.get(Account.class, id);
+            session.remove(account);
+        });
     }
 }
