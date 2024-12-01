1faa44a9fe55ef8f7637d426e53ded9559776837
ПК_Home <wrofdt@ya.ru>
2024-12-01 20:36:07 +0300

    add HibernateConfiguration

---
+++ src/main/java/org/das/config/HibernateConfiguration.java
@@ -0,0 +1,33 @@
+package org.das.config;
+
+import org.das.model.Account;
+import org.das.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.springframework.context.annotation.Bean;
+import java.util.Properties;
+
+@org.springframework.context.annotation.Configuration
+public class HibernateConfiguration {
+
+    @Bean
+    public SessionFactory sessionFactory() {
+        Configuration configuration = new Configuration();
+
+        configuration.addAnnotatedClass(Account.class);
+        configuration.addAnnotatedClass(User.class);
+
+        Properties properties = new Properties();
+        properties.setProperty(Environment.DRIVER, "org.postgresql.Driver");
+        properties.setProperty(Environment.URL, "jdbc:postgresql://localhost:5432/postgres");
+        properties.setProperty(Environment.USER, "postgres");
+        properties.setProperty(Environment.PASS, "Aa123456!");
+        properties.setProperty(Environment.DIALECT, "org.hibernate.dialect.PostgreSQLDialect");
+        properties.setProperty(Environment.SHOW_SQL, "true");
+        properties.setProperty(Environment.HBM2DDL_AUTO, "create-drop");
+        configuration.setProperties(properties);
+
+        return configuration.buildSessionFactory();
+    }
+}
