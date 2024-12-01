d6492f1ef26e70013d3528ed97a59eb960504140
ПК_Home <wrofdt@ya.ru>
2024-12-01 22:27:29 +0300

    refactoring HibernateConfiguration for JPA

--- src/main/java/org/das/config/HibernateConfiguration.java
+++ src/main/java/org/das/config/HibernateConfiguration.java
@@ -6,8 +6,10 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.cfg.Environment;
 import org.springframework.context.annotation.Bean;
+
 import java.util.Properties;
 
+@org.springframework.context.annotation.Configuration
 public class HibernateConfiguration {
 
     @Bean
