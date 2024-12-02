65c7df78e5acddc775f492a7de81a479eba387e6
ПК_Home <wrofdt@ya.ru>
2024-12-02 22:16:46 +0300

    refactoring UserService for JPA

--- src/main/java/org/das/service/UserService.java
+++ src/main/java/org/das/service/UserService.java
@@ -3,11 +3,10 @@ package org.das.service;
 import org.das.model.User;
 
 import java.util.Optional;
-import java.util.UUID;
 
 public interface UserService {
    User create(String login);
    void showAllUsers();
-   Optional<User> getUserById(UUID id);
+   Optional<User> getUserById(Long id);
 
 }
