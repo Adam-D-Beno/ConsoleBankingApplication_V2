c1cd0f9332cef1595a7537c530abf013a5a57048
ПК_Home <wrofdt@ya.ru>
2024-12-05 23:08:38 +0300

    refactoring AccountService for JPA

--- src/main/java/org/das/service/AccountService.java
+++ src/main/java/org/das/service/AccountService.java
@@ -1,12 +1,11 @@
 package org.das.service;
 
 import org.das.model.Account;
-import org.das.model.User;
 
 import java.math.BigDecimal;
 
 public interface AccountService {
-    Account create(User user);
+    Account create(Long userId);
     Account close(Long accountId);
     void deposit(Long accountId, BigDecimal amount);
     void transfer(Long senderId, Long recipientId, BigDecimal amount);
