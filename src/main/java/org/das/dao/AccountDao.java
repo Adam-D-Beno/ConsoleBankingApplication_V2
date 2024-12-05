0342fcaefcea9675475473d362f18211995beef8
ПК_Home <wrofdt@ya.ru>
2024-12-05 18:12:58 +0300

    refactoring AccountDao for JPA

--- src/main/java/org/das/dao/AccountDao.java
+++ src/main/java/org/das/dao/AccountDao.java
@@ -4,7 +4,8 @@ import org.das.model.Account;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import java.util.*;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class AccountDao {
