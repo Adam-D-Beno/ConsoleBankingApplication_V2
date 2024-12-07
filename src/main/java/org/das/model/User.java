7ce1c0ecb0573b2b30b595331d6802f0b4b75c32
ПК_Home <wrofdt@ya.ru>
2024-12-07 14:19:57 +0300

    refactoring User JPA

--- src/main/java/org/das/model/User.java
+++ src/main/java/org/das/model/User.java
@@ -12,7 +12,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "login", unique = true, nullable = false)
+    @Column(name = "login", unique = true, nullable = false, updatable = false)
     private String login;
 
     @OneToMany(mappedBy = "user", fetch = FetchType.EAGER)
